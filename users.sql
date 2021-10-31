74b5ce12ce17fa8899b39324506c48f6723456b7
Eleanor Ozer <eo5fj@virginia.edu>
2021-10-31 12:38:45 -0400

    added users table

---
+++ users.sql
@@ -0,0 +1,58 @@
+CREATE TABLE Users(
+   phone   INTEGER  NOT NULL PRIMARY KEY 
+  ,address VARCHAR(40) NOT NULL
+  ,city    VARCHAR(17) NOT NULL
+  ,county  VARCHAR(20) NOT NULL
+  ,state   VARCHAR(2) NOT NULL
+  ,zip     INTEGER  NOT NULL
+);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (6148019788,'74874 Atlantic Ave','Columbus','Franklin','OH',43215);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (5059773911,'366 South Dr','Las Cruces','Dona Ana','NM',88011);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (2017096245,'45 E Liberty St','Ridgefield Park','Bergen','NJ',7660);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (7329247882,'4 Ralph Ct','Dunellen','Middlesex','NJ',8812);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (2128601579,'2742 Distribution Way','New York','New York','NY',10025);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (5049799175,'426 Wolf St','Metairie','Jefferson','LA',70002);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (2126758570,'128 Bransten Rd','New York','New York','NY',10011);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (8058326163,'17 Morena Blvd','Camarillo','Ventura','CA',93012);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (2108129597,'775 W 17th St','San Antonio','Bexar','TX',78204);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (7854637829,'6980 Dorsett Rd','Abilene','Dickinson','KS',67410);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (5415488197,'2881 Lewis Rd','Prineville','Crook','OR',97754);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (9134134604,'7219 Woodfield Rd','Overland Park','Johnson','KS',66204);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (9072314722,'1048 Main St','Fairbanks','Fairbanks North Star','AK',99708);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (3056067291,'678 3rd Ave','Miami','Miami-Dade','FL',33196);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (9077411044,'20 S Babcock St','Fairbanks','Fairbanks North Star','AK',99712);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (9527682416,'2 Lighthouse Ave','Hopkins','Hennepin','MN',55343);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (6173995124,'38938 Park Blvd','Boston','Suffolk','MA',2128);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (3234532780,'5 Tomahawk Dr','Los Angeles','Los Angeles','CA',90006);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (6083367444,'762 S Main St','Madison','Dane','WI',53711);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (2159079111,'209 Decker Dr','Philadelphia','Philadelphia','PA',19132);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (2124029216,'4486 W O St #1','New York','New York','NY',10003);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (9318756644,'39 S 7th St','Tullahoma','Coffee','TN',37388);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (8039255213,'98839 Hawthorne Blvd #6101','Columbia','Richland','SC',29201);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (6108145533,'71 San Mateo Ave','Wayne','Delaware','PA',19087);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (9088776135,'76 Brooks St #9','Flemington','Hunterdon','NJ',8822);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (5169686051,'4545 Courthouse Rd','Westbury','Nassau','NY',11590);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (2159348655,'14288 Foster Ave #4121','Jenkintown','Montgomery','PA',19046);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (8184234007,'4 Otis St','Van Nuys','Los Angeles','CA',91405);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (4014582547,'65895 S 16th St','Providence','Providence','RI',2909);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (2152119589,'14302 Pennsylvania Ave','Huntingdon Valley','Montgomery','PA',19006);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (4019608259,'201 Hawk Ct','Providence','Providence','RI',2904);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (7322341546,'53075 Sw 152nd Ter #615','Monroe Township','Middlesex','NJ',8831);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (5124863817,'59 N Groesbeck Hwy','Austin','Travis','TX',78731);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (3037247371,'2664 Lewis Rd','Littleton','Douglas','CO',80126);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (4147481374,'59 Shady Ln #53','Milwaukee','Milwaukee','WI',53214);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (2126749610,'3305 Nabell Ave #679','New York','New York','NY',10009);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (9077979628,'18 Fountain St','Anchorage','Anchorage','AK',99515);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (8143935571,'7 W 32nd St','Erie','Erie','PA',16502);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (4109149018,'2853 S Central Expy','Glen Burnie','Anne Arundel','MD',21061);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (2088625339,'74 W College St','Boise','Ada','ID',83707);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (4153152761,'701 S Harrison Rd','San Francisco','San Francisco','CA',94104);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (9192259345,'1088 Pinehurst St','Chapel Hill','Orange','NC',27514);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (6505285783,'30 W 80th St #1995','San Carlos','San Mateo','CA',94070);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (9256473298,'20932 Hedley St','Concord','Contra Costa','CA',94520);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (7403438575,'2737 Pistorio Rd #9230','London','Madison','OH',43140);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (5858668313,'74989 Brandon St','Wellsville','Allegany','NY',14895);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (4105204832,'6 Kains Ave','Baltimore','Baltimore City','MD',21215);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (9733542040,'47565 W Grand Ave','Newark','Essex','NJ',7105);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (7734465569,'4284 Dorigo Ln','Chicago','Cook','IL',60647);
+INSERT INTO Users(phone,address,city,county,state,zip) VALUES (9739273447,'6794 Lake Dr E','Newark','Essex','NJ',7104);
